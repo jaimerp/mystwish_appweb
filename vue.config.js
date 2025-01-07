38879528dacd52d0f15607cfeb0fe68b436cc282
Jaime Rodriguez <jaimerodriguez@Air-de-Jaime.station>
2025-01-07 09:24:35 +0100

    Añadido el copy webpavk plugin para el htaccess en dist

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: '.htaccess', to: '' }
+        ]
+      })
+    ]
+  }
+};
